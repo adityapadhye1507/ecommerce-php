b45d9d3ed6b62b95a7206276871e150b235a24c1
Aditya <adityapadhye1507@gmail.com>
2016-03-28 17:47:12 -0400

    Added files via upload

--- web/dbInfo.php
+++ web/dbInfo.php
@@ -1,6 +1,6 @@
 <?php
-	$host ="localhost";
-	$user = "ap8631";
-	$pass = "fr1end";
-	$db = "ap8631";
+	$host ="sql5.freemysqlhosting.net";
+	$user = "sql5112712";
+	$pass = "Lg8bXclqtZ";
+	$db = "sql5112712";
 ?>
